77fe40bd42b6bde4aa22a1881bcf2f75d28aa80a
NieLamu <mxmxlty@gmail.com>
2019-03-02 18:52:19 +0800

    Update version to 2.0.0

--- renderer.js
+++ renderer.js
@@ -5,24 +5,31 @@
 const serialport = require('serialport')
 const createTable = require('data-table')
 
-serialport.list((err, ports) => {
-  console.log('ports', ports);
-  if (err) {
-    document.getElementById('error').textContent = err.message
-    return
-  } else {
-    document.getElementById('error').textContent = ''
-  }
+const listPorts = ()=>{
+  serialport.list((err, ports) => {
+    console.log('ports', ports);
+    if (err) {
+      document.getElementById('error').textContent = err.message
+      return
+    } else {
+      document.getElementById('error').textContent = 'Ports found.'
+    }
+  
+    if (ports.length === 0) {
+      document.getElementById('error').textContent = 'No ports discovered.'
+      document.getElementById('ports').innerHTML = "";
+      return;
+    }
+  
+    const headers = Object.keys(ports[0])
+    const table = createTable(headers)
+    tableHTML = ''
+    table.on('data', data => tableHTML += data)
+    table.on('end', () => document.getElementById('ports').innerHTML = tableHTML)
+    ports.forEach(port => table.write(port))
+    table.end();
+  })
+}
 
-  if (ports.length === 0) {
-    document.getElementById('error').textContent = 'No ports discovered'
-  }
-
-  const headers = Object.keys(ports[0])
-  const table = createTable(headers)
-  tableHTML = ''
-  table.on('data', data => tableHTML += data)
-  table.on('end', () => document.getElementById('ports').innerHTML = tableHTML)
-  ports.forEach(port => table.write(port))
-  table.end();
-})
+listPorts();
+document.getElementById("list").onclick = listPorts;
